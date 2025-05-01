09cff0e4234d03678dc6e807ee9a8f533a3d664e
“shou9129” <takeuchi.shoutaro9129@gmail.com>
2025-05-02 00:38:41 +0900

    update: 一覧画面　電話用・マップ用ボタンを追加

--- lib/widgets/contact_tile.dart
+++ lib/widgets/contact_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/contact.dart';
 import 'dart:developer' as developer;
+import 'package:flutter/material.dart';
 
 class ContactTile extends StatelessWidget {
   final Contact contact;
@@ -30,31 +31,45 @@ class ContactTile extends StatelessWidget {
     return Column(
       children: [
         _buildSeparator(),
-        GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () => _call(contact.phoneNumber),
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 16.0,
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    contact.name,
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: CupertinoColors.label,
-                      fontWeight: FontWeight.normal,
-                      decoration: TextDecoration.none,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  contact.name,
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: CupertinoColors.label,
+                    fontWeight: FontWeight.normal,
+                    decoration: TextDecoration.none,
                   ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
                 ),
-              ],
-            ),
+              ),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                minSize: 30,
+                onPressed: () => _call(contact.phoneNumber),
+                child: Icon(
+                  Icons.circle,
+                  size: 11,
+                  color: CupertinoColors.activeBlue,
+                ),
+              ),
+              SizedBox(width: 5.0),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                minSize: 30,
+                onPressed: () => _call(contact.phoneNumber),
+                child: Icon(
+                  Icons.circle,
+                  size: 11,
+                  color: CupertinoColors.activeGreen,
+                ),
+              ),
+            ],
           ),
         ),
       ],
